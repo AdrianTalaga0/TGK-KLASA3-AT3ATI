41229e5ea3d3b1e1c22c90b4d204dc1b5b3ac775
AdrianTalaga0 <adriantalaga2006@gmail.com>
2023-11-16 13:25:26 +0100

    Added new code
    .

--- UFO-KLASA3-AT3ATI/Assets/Scripts/NewBehaviourScript.cs
+++ UFO-KLASA3-AT3ATI/Assets/Scripts/NewBehaviourScript.cs
@@ -1,7 +1,11 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.SocialPlatforms.Impl;
 using UnityEngine.UI;
+
+
 
 public class NewScript : MonoBehaviour
 {
@@ -9,6 +13,7 @@ public class NewScript : MonoBehaviour
     Rigidbody2D rb2d;
     private int count = 0;
     public Text score_02;
+    [SerializeField] Text winText;
     void Start()
     {
         rb2d = GetComponent<Rigidbody2D>();
@@ -19,15 +24,27 @@ public class NewScript : MonoBehaviour
         float moveHorizontal = Input.GetAxis("Horizontal");
         float moveVertical = Input.GetAxis("Vertical");
         Vector2 movement = new Vector2(moveHorizontal, moveVertical);
-        rb2d.AddForce(movement*speed);
+        rb2d.AddForce(movement * speed);
     }
     private void OnTriggerEnter2D(Collider2D obiekt)
     {
-       if(obiekt.gameObject.CompareTag("PickUp"))
-       {
+        if (obiekt.gameObject.CompareTag("PickUp"))
+        {
             count++;
+            score_02.text = count.ToString();
             Destroy(obiekt.gameObject);
-            
-       }
+            if (count >= 5)
+            {
+                winText.gameObject.SetActive(true);
+                int nextSceneIndex = SceneManager.GetActiveScene().buildIndex + 1;
+                StartCoroutine(LoadNextSceneAfterDelay(5f, nextSceneIndex));
+            }
+        }
+    }
+
+    IEnumerator LoadNextSceneAfterDelay(float delay, int sceneIndex)
+    {
+        yield return new WaitForSeconds(delay);
+        SceneManager.LoadScene(sceneIndex);
     }
 }
